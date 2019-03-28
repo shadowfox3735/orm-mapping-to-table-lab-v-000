abdad60e74ff3142ee085bc083b1529b5573874f
shadowfox3735 <normanpan180@gmail.com>
2019-03-28 18:43:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     INSERT INTO students(name, grade) VALUES (?,?)
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1")[0][0]
   end
 
